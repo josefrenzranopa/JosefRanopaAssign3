66f1e8d6f7ca92d7e5997d7cadf15427c7bf2fe7
josefrenzranopa <n01312587@humbermail.ca>
2020-11-29 23:21:58 -0500

    made ranopa_content.xml ranopa_header.xml ranopa_toolbar.xml and ranopa_menu.xml

--- JosefLab3/src/main/java/josef/ranopa/n01312587/JosefActivity.java
+++ JosefLab3/src/main/java/josef/ranopa/n01312587/JosefActivity.java
@@ -9,6 +9,6 @@ public class JosefActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_josef);
     }
 }
