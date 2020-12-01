e7c46d59c818cf6c4db6f854963240ea2b942823
josefrenzranopa <n01312587@humbermail.ca>
2020-11-30 21:21:02 -0500

    Fixed onNavagigationItemSelected if statements

--- JosefLab3/src/main/java/josef/ranopa/n01312587/JoHome.java
+++ JosefLab3/src/main/java/josef/ranopa/n01312587/JoHome.java
@@ -8,35 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link JoHome#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class JoHome extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
     public JoHome() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment JoHome.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static JoHome newInstance(String param1, String param2) {
         JoHome fragment = new JoHome();
         Bundle args = new Bundle();
@@ -58,7 +45,7 @@ public class JoHome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.jo_home, container, false);
+        View view = inflater.inflate(R.layout.jo_home,container,false);
+        return view;
     }
 }
--- JosefLab3/src/main/java/josef/ranopa/n01312587/JosefActivity.java
+++ JosefLab3/src/main/java/josef/ranopa/n01312587/JosefActivity.java
@@ -1,21 +1,27 @@
 package josef.ranopa.n01312587;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class JosefActivity extends AppCompatActivity {
+public class JosefActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
     NavigationView navigationView;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +32,51 @@ public class JosefActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.josefdrawer);
         navigationView = findViewById(R.id.josefnavigationView);
+        navigationView.setNavigationItemSelectedListener(this);
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         actionBarDrawerToggle.syncState();
 
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.josef_container,new JoHome());
+        fragmentTransaction.commit();
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        if(menuItem.getItemId() == R.id.joshome){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.josef_container,new JoHome());
+            fragmentTransaction.commit();
+        }
+
+        if(menuItem.getItemId() == R.id.josjosef){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.josef_container,new JosDown());
+            fragmentTransaction.commit();
+        }
+
+        if(menuItem.getItemId() == R.id.josranopa){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.josef_container,new RaSrv());
+            fragmentTransaction.commit();
+        }
+
+        if(menuItem.getItemId() == R.id.jossetting){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.josef_container,new RaSet());
+            fragmentTransaction.commit();
+        }
+
+        return true;
     }
 }
