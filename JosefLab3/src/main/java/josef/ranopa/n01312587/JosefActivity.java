0cda50823249c6c360ffe8c09fdabc8d271623f4
josefrenzranopa <n01312587@humbermail.ca>
2020-11-30 19:53:22 -0500

    added vector assets and code on JosefActivity.java

--- JosefLab3/src/main/java/josef/ranopa/n01312587/JosefActivity.java
+++ JosefLab3/src/main/java/josef/ranopa/n01312587/JosefActivity.java
@@ -1,14 +1,37 @@
 package josef.ranopa.n01312587;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class JosefActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_josef);
+        toolbar = findViewById(R.id.joseftoolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.josefdrawer);
+        navigationView = findViewById(R.id.josefnavigationView);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+
     }
 }
