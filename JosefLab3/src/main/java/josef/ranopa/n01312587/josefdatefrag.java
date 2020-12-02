072020087016329695bb7d3a3cc5e0f1be8ad5f3
josefrenzranopa <n01312587@humbermail.ca>
2020-12-02 13:58:40 -0500

    made josefdatefrag.java

---
+++ JosefLab3/src/main/java/josef/ranopa/n01312587/josefdatefrag.java
@@ -0,0 +1,35 @@
+package josef.ranopa.n01312587;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class josefdatefrag extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        final Calendar calendar = Calendar.getInstance();
+        int yy = calendar.get(Calendar.YEAR);
+        int mm = calendar.get(Calendar.MONTH);
+        int dd = calendar.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), this, yy, mm, dd);
+    }
+
+    public void onDateSet(DatePicker view, int yy, int mm, int dd) {
+        populateSetDate(yy, mm+1, dd);
+    }
+    public void populateSetDate(int year, int month, int day) {
+        TextView jostv= (TextView)getActivity(). findViewById(R.id.jostvdatepicker);
+        jostv.setText(month+"/"+day+"/"+year);
+    }
+
+}
