5a77a65d972c110367ac2e035fece6c97eed3d91
josefrenzranopa <n01312587@humbermail.ca>
2020-12-02 22:32:44 -0500

    brightness done

--- JosefLab3/src/main/java/josef/ranopa/n01312587/JosDown.java
+++ JosefLab3/src/main/java/josef/ranopa/n01312587/JosDown.java
@@ -11,34 +11,6 @@ import android.view.ViewGroup;
 
 public class JosDown extends Fragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public JosDown() {
-
-    }
-
-    public static JosDown newInstance(String param1, String param2) {
-        JosDown fragment = new JosDown();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
--- JosefLab3/src/main/java/josef/ranopa/n01312587/RaSet.java
+++ JosefLab3/src/main/java/josef/ranopa/n01312587/RaSet.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -27,6 +30,7 @@ public class RaSet extends Fragment  {
     RadioButton radioButton;
     SeekBar seekBar;
     TextView textP;
+    int brightness;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,13 +38,70 @@ public class RaSet extends Fragment  {
         View view = inflater.inflate(R.layout.ra_set, container, false);
         switchCompat = (SwitchCompat) view.findViewById(R.id.josswitch);
 
-        seekBar = (SeekBar) view.findViewById(R.id.seekBar);
+        seekBar = (SeekBar) view.findViewById(R.id.josseekBar);
         textP = (TextView) view.findViewById(R.id.josbrightnesstv);
         ContentResolver resolver = getActivity().getContentResolver();
         Window window = getActivity().getWindow();
 
         seekBar.setMax(255);
         seekBar.setKeyProgressIncrement(1);
+
+        try
+        {
+            //Get the current system brightness
+            brightness = Settings.System.getInt(resolver, Settings.System.SCREEN_BRIGHTNESS);
+        }
+        catch (Settings.SettingNotFoundException e)
+        {
+            //Throw an error case it couldn't be retrieved
+            Log.e("Error", "Cannot access system brightness");
+            e.printStackTrace();
+        }
+
+        seekBar.setProgress(brightness);
+
+        seekBar.setProgress(brightness);
+
+        //Register OnSeekBarChangeListener, so it can actually change values
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+        {
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+                //Set the system brightness using the brightness variable value
+                Settings.System.putInt(resolver, Settings.System.SCREEN_BRIGHTNESS, brightness);
+                //Get the current window attributes
+                ViewGroup.LayoutParams layoutpars = window.getAttributes();
+                //Set the brightness of this window
+                ((WindowManager.LayoutParams) layoutpars).screenBrightness = brightness / (float)255;
+                //Apply attribute changes to this window
+                window.setAttributes((WindowManager.LayoutParams) layoutpars);
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+                //Nothing handled here
+            }
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                //Set the minimal brightness level
+                //if seek bar is 20 or any value below
+                if(progress<=20)
+                {
+                    //Set the brightness to 20
+                    brightness=20;
+                }
+                else //brightness is greater than 20
+                {
+                    //Set brightness variable based on the progress bar
+                    brightness = progress;
+                }
+                //Calculate the brightness percentage
+                float perc = (brightness /(float)255)*100;
+                //Set the brightness percentage
+                textP.setText((int)perc +" %");
+            }
+        });
 
 
         radioGroup = (RadioGroup) view.findViewById(R.id.josradioGroup);
@@ -58,6 +119,9 @@ public class RaSet extends Fragment  {
                         break;
                     case R.id.josmagenta:
                         getActivity().getWindow().getDecorView().setBackgroundColor(Color.MAGENTA);
+                        break;
+                    case R.id.josdefault:
+                        getActivity().getWindow().getDecorView().setBackgroundColor(Color.WHITE);
                         break;
                     default:
                 }
